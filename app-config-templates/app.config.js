bed7f39d15af03cf541b3857973c8391f7bcc2a5
Mark Noble <mdnoble73@gmail.com>
2024-12-27 09:46:28 -0700

    Comment out debugging code

--- app-config-templates/app.config.js
+++ app-config-templates/app.config.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-     console.log("Returning app configuration");
+     //console.log("Returning app configuration");
      // noinspection UnnecessaryLocalVariableJS
      let config = {{LOAD_APP_CONFIG}};
      //console.log(config);
